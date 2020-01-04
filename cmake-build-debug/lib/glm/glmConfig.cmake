091defc617a90c4878ea0d468ea35ecdf85fbe4b
ABDURRAHMAN MERT SAFAK <e2171981@ceng.metu.edu.tr>
2020-01-04 16:34:09 +0300

    no message

--- cmake-build-debug/lib/glm/glmConfig.cmake
+++
@@ -1,6 +0,0 @@
-set(GLM_VERSION "0.9.8")
-set(GLM_INCLUDE_DIRS "C:/Users/selim/Desktop/Fall 2019-2020/ceng477/hw3/Ceng477-HW3/lib/glm")
-
-if (NOT CMAKE_VERSION VERSION_LESS "3.0")
-    include("${CMAKE_CURRENT_LIST_DIR}/glmTargets.cmake")
-endif()
--- cmake-build-debug/lib/glm/test/bug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/selim/Desktop/Fall 2019-2020/ceng477/hw3/Ceng477-HW3/lib/glm/test/bug
-# Build directory: C:/Users/selim/Desktop/Fall 2019-2020/ceng477/hw3/Ceng477-HW3/cmake-build-debug/lib/glm/test/bug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

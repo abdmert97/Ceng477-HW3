643c3827a8b0335a0e1d141ed9277290b54ef665
Selim Sahin <e2171999@ceng.metu.edu.tr>
2020-01-14 15:25:04 +0300

    Refactor

--- SphereMap.cpp
+++ SphereMap.cpp
@@ -36,53 +36,47 @@ void SphereMap::Render(const char *coloredTexturePath,const char *greyTexturePat
     setText(shaderID);
 //---------------------------------------
     // Set Vertices
-    float x, y, z, xy;                              // vertex position
-    float nx, ny, nz, lengthInv = 1.0f / radius;    // vertex normal
+                             // vertex position
+       // vertex normal
     float s, t;                                     // vertex texCoord
 
     float sectorStep = 2 *  PI / sectorCount;
     float stackStep = PI / stackCount;
-    float sectorAngle, stackAngle;
 
     for(int i = 0; i <= stackCount; ++i)
     {
-        stackAngle = PI / 2 - i * stackStep;        // starting from pi/2 to -pi/2
-        xy = radius * cosf(stackAngle);             // r * cos(u)
-        z = radius * sinf(stackAngle);              // r * sin(u)
+        float stackAngle = PI / 2 - i * stackStep;        // starting from pi/2 to -pi/2
+        float xy = radius * cosf(stackAngle);             // r * cos(u)
+        float z = radius * sinf(stackAngle);              // r * sin(u)
 
         // add (sectorCount+1) vertices per stack
         // the first and last vertices have same position and normal, but different tex coords
         for(int j = 0; j <= sectorCount; ++j)
         {
-            sectorAngle = j * sectorStep;           // starting from 0 to 2pi
+            float sectorAngle = j * sectorStep;           // starting from 0 to 2pi
 
             // vertex position (x, y, z)
-            x = xy * cosf(sectorAngle);             // r * cos(u) * cos(v)
-            y = xy * sinf(sectorAngle);             // r * cos(u) * sin(v)
+           float x = xy * cosf(sectorAngle);             // r * cos(u) * cos(v)
+           float y = xy * sinf(sectorAngle);             // r * cos(u) * sin(v)
             vertices.push_back(x);
             vertices.push_back(y);
             vertices.push_back(z);
 
             // normalized vertex normal (nx, ny, nz)
-            nx = x * lengthInv;
-            ny = y * lengthInv;
-            nz = z * lengthInv;
-            normals.push_back(nx);
-            normals.push_back(ny);
-            normals.push_back(nz);
+            normals.push_back(x * (1 / radius));
+            normals.push_back(y * (1 / radius));
+            normals.push_back(z * (1 / radius));
 
             // vertex tex coord (s, t) range between [0, 1]
-            s = (float)j / sectorCount;
-            t = (float)i / stackCount;
-            texCoords.push_back(s);
-            texCoords.push_back(t);
+            texCoords.push_back((float)j / sectorCount);
+            texCoords.push_back((float)i / stackCount);
         }
     }
-    int k1, k2;
+
     for(int i = 0; i < stackCount; ++i)
     {
-        k1 = i * (sectorCount + 1);     // beginning of current stack
-        k2 = k1 + sectorCount + 1;      // beginning of next stack
+        int k1 = i * (sectorCount + 1);     // beginning of current stack
+        int k2 = k1 + sectorCount + 1;      // beginning of next stack
 
         for(int j = 0; j < sectorCount; ++j, ++k1, ++k2)
         {
